3004b0b4b8ebddc56d2ad2b7fd04797ec5ee77a6
malcosll <162439457+malcosll@users.noreply.github.com>
2024-03-22 08:01:36 -0300

    Add files via upload

---
+++ string.js
@@ -0,0 +1,13 @@
+const txt1 = "Olá mundo.";
+const txt2 = "Sua senha é: ";
+const senha = "SenhaSeguraDoMarcos";
+const stringdenumeros = "34567";
+
+const citacao = "seu nome é"
+const meuNome = "Marcos. "
+
+console.log (txt1)
+console.log (txt2)
+console.log (senha)
+console.log (stringdenumeros)
+console.log (citacao, meuNome)
